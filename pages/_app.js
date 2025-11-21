8e56ec53896575495cc197dc9988adf16147e677
pixelavii <pixelavii007@gmail.com>
2025-11-21 16:43:01 +0530

    Review component removal

--- pages/_app.js
+++ pages/_app.js
@@ -10,7 +10,6 @@ export default function App({ Component, pageProps }) {
     <Provider store={store}>
       <Navbar />
       <Component {...pageProps} />
-      <ReviewCarousel />
       <Footer />
     </Provider>
   );
